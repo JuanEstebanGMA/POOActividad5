f5fe963d7f5af651b0c26ad8034ca7af7aac6cfb
JuanEstebanGMA <jugomezac@unal.edu.co>
2023-11-18 18:22:44 -0500

    POO822

---
+++ src/pooep8_2/POOEP8_2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pooep8_2;
+
+/**
+ *
+ * @author Juan
+ */
+public class POOEP8_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+    
+}
