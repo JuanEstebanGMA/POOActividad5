cfcc92d6cc28c18c7a5d9cc99ac9f23d85836089
Valentina Rodriguez Henao <144085605+valtinarh@users.noreply.github.com>
2023-11-18 20:26:56 -0500

    POOE82

---
+++ POO_E82/src/main/java/Notas/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Notas;
+
+/**
+ *
+ * @author Juan
+ */
+public class Principal {
+    public static void main(String[] args) {
+        VentanaPrincipal form = new VentanaPrincipal();
+        form.setVisible(true);
+    }
+}
